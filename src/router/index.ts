a3861b882d1386e3387f452d98e6cdc5c50b0876
Oleksandr Sokolianskiy <alexsokolianskiy@gmail.com>
2024-06-16 16:19:51 +0300

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,10 +7,6 @@ import { useUIStore } from '@/stores/ui';
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/:pathMatch(.*)*',
-      component: () => import('@/views/pages/maintenance/error/Error404Page.vue')
-    },
     MainRoutes,
     AuthRoutes
   ]
